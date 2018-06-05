bf9489ed2f0be8687b6b1e828030a82d656a3c67
freddy-mrqz <freddymrqz28@gmail.com>
2018-06-05 00:03:41 -0500

    name contains filter

--- src/planner/filters.py
+++ src/planner/filters.py
@@ -2,6 +2,7 @@ from planner.models.course import Course
 import django_filters
 
 class CourseFilter(django_filters.FilterSet):
+  name = django_filters.CharFilter(lookup_expr='icontains')
   class Meta:
     model = Course
     fields = ['subject', 'number', 'name']
