b7c16a03377a450da788aecb7c5ec567e1eeb62b
freddy-mrqz <freddymrqz28@gmail.com>
2018-06-05 00:41:00 -0500

    term field filter works

--- src/planner/filters.py
+++ src/planner/filters.py
@@ -3,6 +3,7 @@ import django_filters
 
 class CourseFilter(django_filters.FilterSet):
   name = django_filters.CharFilter(lookup_expr='icontains')
+  
   class Meta:
     model = Course
-    fields = ['subject', 'number', 'name']
+    fields = ['subject', 'number', 'name', 'term']
