eb2e2f2627942862b1904852e1ccc189e5e3165e
Connor Taylor <cptaylor2014@gmail.com>
2018-06-05 20:18:24 -0500

    final views created

--- src/planner/views/__init__.py
+++ src/planner/views/__init__.py
@@ -1,4 +1,4 @@
 from .index import IndexView
-from .student import StudentList, StudentForm, CourseBrowser, StudentStep2, StudentFinish
+from .student import StudentForm, StudentStep2, StudentFinish, StudentSave, StudentLoad
 from .login import LoginView
 from .faculty import FacultyForm, FacultyLookup, FacultyStep2, FacultyFinish
