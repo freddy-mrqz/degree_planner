68cf663101f298aef1f4f3f33bbdced6c13c6e2a
Connor Taylor <cptaylor2014@gmail.com>
2018-06-05 21:10:14 -0500

    still updating IO

--- src/planner/views/IO.py
+++ src/planner/views/IO.py
@@ -28,7 +28,7 @@ def string_to_path(start,string):
         try:
             courses.append(Course.objects.get(course_id=int(course)))
         except:
-            raise Exception("requires a list of correct course numbers")
+            course.append("empty")
         if len(courses) == num:
             path.append(Term(seasons[index],courses))
             term_num += 1
